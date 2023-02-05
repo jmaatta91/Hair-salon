0a746119f3ab16f656993988a3be5919eff5ef49
jmaatta91 <jmaatta91@outlook.com>
2023-02-05 10:27:08 +0000

    Added style and javascript functionality

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-60px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+var menuBtn = document.getElementById("menu__toggle");
+menuBtn.addEventListener("click", showMenuBar);
+
+function showMenuBar(){
+  document.getElementById("navbar").style.top = "0";
+}
