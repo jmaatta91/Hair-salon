8e70ebb7da2e09ff0ea4ad5003f42243dec516bb
jmaatta91 <jmaatta91@outlook.com>
2023-02-18 18:41:34 +0000

    Added javascript for handling of menu when list item is pressed and some minor changes in style.css

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,11 +17,27 @@ function showMenuBar(){
   document.getElementById("navbar").style.top = "0";
 }
 
-$(window).scroll(function() {
-  var scrolledY = $(window).scrollTop();
-  $('#main-box').css('background-position', 'left ' + ((scrolledY)) + 'px');
+
+
+const linksContainer = document.querySelector('.menu__box');
+
+linksContainer.addEventListener('click', (e) => {
+  if (e.target.matches('.menu__item')) {
+    // disable default behavior
+    e.preventDefault();
+
+    // close the side-bar menu
+    closeSideMenu();
+
+    // change the location
+   window.location.href = e.target.href;
+  }
 });
 
+function  closeSideMenu() {
+  const sideMenuToggle = document.querySelector('#menu__toggle');
+  sideMenuToggle.click();
+}
 
 
 
